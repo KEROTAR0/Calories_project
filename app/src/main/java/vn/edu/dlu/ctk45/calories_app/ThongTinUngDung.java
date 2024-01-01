880cfc82c37609fe2c1f84fa1749ebc5d616a675
Buro <betyarioy@gmail.com>
2024-01-01 22:43:40 +0700

    bua sang/trua/toi

--- app/src/main/java/vn/edu/dlu/ctk45/calories_app/ThongTinUngDung.java
+++ app/src/main/java/vn/edu/dlu/ctk45/calories_app/ThongTinUngDung.java
@@ -1,10 +1,18 @@
 package vn.edu.dlu.ctk45.calories_app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -24,6 +32,25 @@ public class ThongTinUngDung extends Fragment {
                 getActivity().onBackPressed();
             }
         });
+        ImageView congThucNauAn = rootView.findViewById(R.id.cong_thuc_nau_an);
+        ImageView phuongPhapGiamCan = rootView.findViewById(R.id.phuong_phap_giam_can);
+        String link1 = "https://monngonmoingay.com/tim-kiem-mon-ngon/";
+        String link2 = "https://suckhoedoisong.vn/cac-phuong-phap-giam-can-khoa-hoc-16922092711175053.htm";
+
+        setClickableImage(congThucNauAn, link1);
+        setClickableImage(phuongPhapGiamCan, link2);
+
         return rootView;
+    }
+
+    private void setClickableImage(ImageView imageView, final String link) {
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Mở đường liên kết: " + link, Toast.LENGTH_SHORT).show();
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
